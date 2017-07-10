0a81f8450c710eda9b114f86dbc1cf60e0d00547
jayeshvaghela <vaghelaj@sheridancollege.ca>
2017-07-10 02:09:15 -0400

    Delete Button
    The delete task button works. Finally after using event delegation.

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
+'use strict';
+
 var formGroup = document.getElementById('formGroup');
 var addButton = document.getElementById('addButton');
-var deleteButton = document.querySelectorAll('.trash');
 var list = document.getElementById('list');
 
 addButton.addEventListener('click', Brain, false);
 // TODO: Make it so that when the user hits the enter button, it also adds the item value into the list.
 
-for (var i = 0; i < deleteButton.length; i++) {
-  deleteButton[i].onclick = print;
-}
+list.addEventListener('click', function(e) {
+  if(e.target && e.target.nodeName == 'I') {
+    e.target.parentNode.remove();
+  }
+});
 
-function print() {
-  console.log("I");
-}
-
-function Brain() {
+function Brain(e) {
   var input = document.getElementById('input').value;
 
   //Each new listItem will contain the latest user input.
   var listItem = document.createElement('li');
+  listItem.className = "listItem"
   var listItemValue = document.createTextNode(input);
 
   //Creating a 'checkbox' element that will be added along side the input string. Allowing the user to check or uncheck.
@@ -26,9 +26,8 @@ function Brain() {
   checkBox.type= "checkbox";
 
   //Creating a span that will contain 'i' element with the given class name for the font icon.
-  var removeItem = document.createElement('p');
-  removeItem.className = "trash"
-  removeItem.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>'
+  var removeItem = document.createElement('i');
+  removeItem.className = "fa fa-trash"
 
   //Checking if the input field is empty or not. If yes, printing a message in the console otherwise it the data is appended to the body.
   if(input === "") {
@@ -39,6 +38,7 @@ function Brain() {
     listItem.appendChild(listItemValue); //Lastly will append the listItem to the list.
     listItem.appendChild(removeItem);
     list.appendChild(listItem);
+
   }
 
   formGroup.reset(); //This line resets the form back to original state once the button is clicked.
